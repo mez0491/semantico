4bafcdb5c352c9db8579a6c5664d5c50ede070ed
mez0491 <238040573+mez0491@users.noreply.github.com>
2025-10-20 18:42:43 -0600

    corrections to mobile menu

---
+++ script/nav_button.js
@@ -0,0 +1,82 @@
+document.addEventListener("DOMContentLoaded",setup); 
+
+var menu_button;
+var side_bar;
+
+var is_menu_open = false;
+
+function setup(){
+    /*Encontrar los elementos de menu*/
+    menu_button = document.getElementById("nav_menu_button");
+    side_bar = document.getElementById("sidebar");
+    
+    /*Asignar botones para cerrar menu */
+    assign_buttons();
+
+    if (menu_button){
+
+        /*Checar si esta en dispositivo movil*/
+        const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+        /*Conectar la funcion de touchend solo si esta en dispositivo movil*/
+        if (isMobile){
+            menu_button.addEventListener("touchend",toggle_menu);
+        }else{
+            menu_button.addEventListener("click",toggle_menu);
+        }
+    }
+}
+function toggle_menu(){
+    /*Checar si el boton de navegacion es visible para activar el menu*/
+    const estilos_computados = window.getComputedStyle(menu_button);
+
+    if (estilos_computados.display != "none"){
+        switch (is_menu_open) {
+            case false:
+                open_menu();    
+                break;
+            case true:
+                close_menu();    
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+function open_menu(){
+    if (is_menu_open == false){
+        console.log("Menu abierto!");
+        make_side_bar_small();
+        is_menu_open = true;
+    }
+}
+function close_menu(){
+    if (is_menu_open == true){
+        console.log("Menu cerrado!");
+        make_side_bar_big();
+        is_menu_open = false;
+    }
+}
+
+function make_side_bar_small(){
+    side_bar.classList.add("nav_menu_side");
+    side_bar.classList.remove("nav_menu_big");
+}
+
+function make_side_bar_big(){
+    side_bar.classList.remove("nav_menu_side");
+    side_bar.classList.add("nav_menu_big");
+}
+
+function assign_buttons(){
+    const buttons = document.getElementsByClassName("sidemenu_button");
+
+    for (index = 0; index < buttons.length; index++) {
+        const element = buttons[index];
+        element.addEventListener("click",close_menu);
+        element.addEventListener("touchend",close_menu);
+        
+    }
+}
+
